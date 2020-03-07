afacd385eb0eb2f19220b3be5fd7dd6b47149913
Will Page <compenguy@gmail.com>
2020-03-07 13:04:54 -0800

    Simplify error declaration with thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: MIT
 use crate::json::errors::JsonError;
 
+use thiserror::Error;
+
 /// A general, crate-wide result type.
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -10,66 +12,23 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// this API.  The PandoraJsonRequestError variant wraps a separate error type
 /// (json::errors::JsonError) that captures errors returned by the Pandora
 /// JSON API.
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
     /// Wraps serde_json serialization/deserializaiton errors
-    JsonSerializationError(serde_json::error::Error),
+    #[error("JSON serialization error: {0}")]
+    JsonSerializationError(#[from] serde_json::error::Error),
     /// Wraps reqwest errors
-    HttpIoError(reqwest::Error),
+    #[error("HTTP I/O error: {0}")]
+    HttpIoError(#[from] reqwest::Error),
     /// Wraps another error type that describes API errors returned by the
     /// Pandora JSON API
-    PandoraJsonRequestError(JsonError),
+    #[error("Pandora API error: {0}")]
+    PandoraJsonRequestError(#[from] JsonError),
     /// Invalid/unsupported audio format was specified
+    #[error("Invalid/unsupported audio format: {0}")]
     InvalidAudioFormat(String),
     /// Invalid/unsupported gender string was specified
+    #[error("Invalid/unsupported gender value: {0}")]
     InvalidUserGender(String),
 }
 
-impl std::error::Error for Error {
-    /// Return the source for this error, if any
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Error::JsonSerializationError(e) => Some(e),
-            Error::HttpIoError(e) => Some(e),
-            Error::PandoraJsonRequestError(e) => Some(e),
-            Error::InvalidAudioFormat(_) => None,
-            Error::InvalidUserGender(_) => None,
-        }
-    }
-}
-
-impl std::fmt::Display for Error {
-    /// Format this error for display
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Error::JsonSerializationError(e) => write!(f, "JSON serialization error: {}", e),
-            Error::HttpIoError(e) => write!(f, "HTTP I/O error: {}", e),
-            Error::PandoraJsonRequestError(e) => write!(f, "Pandora API error: {}", e),
-            Error::InvalidAudioFormat(fmt) => {
-                write!(f, "Invalid/unsupported audio format: {}", fmt)
-            }
-            Error::InvalidUserGender(fmt) => write!(f, "Invalid/unsupported gender value: {}", fmt),
-        }
-    }
-}
-
-impl From<JsonError> for Error {
-    /// Create the appropriate instance of this error type from JsonErrors.
-    fn from(err: JsonError) -> Self {
-        Error::PandoraJsonRequestError(err)
-    }
-}
-
-impl From<serde_json::error::Error> for Error {
-    /// Create the appropriate instance of this error type from serde_json errors.
-    fn from(err: serde_json::error::Error) -> Self {
-        Error::JsonSerializationError(err)
-    }
-}
-
-impl From<reqwest::Error> for Error {
-    /// Create the appropriate instance of this error type from reqwest errors.
-    fn from(err: reqwest::Error) -> Self {
-        Error::HttpIoError(err)
-    }
-}
